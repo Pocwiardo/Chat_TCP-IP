5172924be5491f11386711205928728edc67591a
Jakub Poćwiardowski <80002864+Pocwiardo@users.noreply.github.com>
2023-03-01 14:40:16 +0100

    Update server.py

--- server.py
+++ server.py
@@ -1,5 +1,32 @@
 import socket
 import threading
+import select
+from PIL import Image
+import time
+
+def convert_to_ascii_art(image, output_width=50):
+    # Skonwertuj obraz na skalę szarości
+    image = image.convert('L')
+
+    # Zdefiniuj listę znaków ASCII, które będą reprezentować różne poziomy jasności
+    ascii_chars = ["@", "#", "S", "%", "?", "*", "+", ";", ":", ",", "."]
+
+    # Oblicz wysokość obrazu na podstawie proporcji
+    width, height = image.size
+    output_height = int(height / width * output_width)
+    image = image.resize((output_width, output_height))
+
+    # Podziel obraz na bloki pikseli i zamień każdy blok na odpowiadający mu znak ASCII
+    ascii_art = ''
+    for i in range(0, output_height, 1):
+        for j in range(0, output_width, 1):
+            pixel_value = 255 - image.getpixel((j, i))
+            ascii_index = int(pixel_value / 25)
+            ascii_char = ascii_chars[ascii_index]
+            ascii_art += ascii_char
+        ascii_art += '\n'
+    #print(ascii_art)
+    return ascii_art
 
 def handle_client(client_socket):
     while True:
@@ -7,7 +34,17 @@ def handle_client(client_socket):
             message = client_socket.recv(1024).decode('utf-8')
             if message == 'q':
                 break
-            print(f"Received message from client: {message}")
+            #elif message.startswith('send_file'):
+            #    # Pobierz nazwę pliku z wiadomości
+            #    filename = message.split()[1]
+
+                # Otwórz plik do zapisu
+           #     with open(f'server_{filename}.txt', 'w') as f:
+                    # Zapisz otrzymany ASCII-art do pliku
+            #        f.write(client_socket.recv(1024).decode('utf-8'))
+            #print(f"Received message from client: {message}")
+            else:
+                print(message)
         except:
             break
 
@@ -32,11 +69,29 @@ def main():
 
 def send_message(client_socket):
     while True:
-        #message = input("Message: ")
         message = input()
-        client_socket.send(message.encode('utf-8'))
+
         if message == 'q':
             break
+        elif message.startswith('send_file'):
+            # Pobierz nazwę pliku z wiadomości
+            filename = message.split()[1]
+
+            # Wczytaj plik graficzny
+            img = Image.open(filename)
+
+            # Konwertuj obraz na ASCII-art
+            ascii_art = convert_to_ascii_art(img)
+
+            # Prześlij ASCII-art do serwera
+            #print(ascii_art)
+            for line in ascii_art.split('\n'):
+                if line:
+                    client_socket.send(line.encode('utf-8'))
+                    time.sleep(0.02)  # opóźnienie dla lepszej czytelności
+                    # server_socket.send('\n'.encode('utf-8'))
+        else:
+            client_socket.send(message.encode('utf-8'))
 
     client_socket.close()
 
