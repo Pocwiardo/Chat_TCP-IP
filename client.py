b44a01a4b2c45ec3a6c1d38ceaae94ed60961fe0
Jakub Poćwiardowski <80002864+Pocwiardo@users.noreply.github.com>
2023-03-04 20:24:54 +0100

    okienka

--- client.py
+++ client.py
@@ -1,9 +1,11 @@
+
 import socket
 import threading
 import select
+import sys
+from PySide2.QtWidgets import QApplication, QMainWindow, QWidget, QVBoxLayout, QHBoxLayout, QTextEdit, QLineEdit, QPushButton, QFileDialog
 from PIL import Image
 import time
-
 
 def convert_to_ascii_art(image, output_width=50):
     # Skonwertuj obraz na skalę szarości
@@ -29,59 +31,113 @@ def convert_to_ascii_art(image, output_width=50):
     #print(ascii_art)
     return ascii_art
 
-def handle_server(server_socket):
-    while True:
-        try:
-            # Użyj funkcji select, aby oczekiwać na dane do odczytu z serwera
-            ready_to_read, _, _ = select.select([server_socket], [], [], 0.1)
-            if server_socket in ready_to_read:
-                message = server_socket.recv(1024).decode('utf-8')
-                if message == 'q':
-                    break
-                #print(f"Received message from server: {message}")
-                print(message)
-        except:
-            break
 
-    server_socket.close()
 
-def main():
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_socket.connect(('localhost', 9999))
 
-    receive_thread = threading.Thread(target=handle_server, args=(server_socket,))
-    receive_thread.start()
+class ChatWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
 
-    send_thread = threading.Thread(target=send_message, args=(server_socket,))
-    send_thread.start()
+        # Utwórz okno
+        self.setWindowTitle('Chat')
+        self.setGeometry(100, 100, 800, 600)
 
-def send_message(server_socket):
-    while True:
-        message = input()
+        # Utwórz widgety
+        self.message_history = QTextEdit()
+        self.message_input = QLineEdit()
+        self.send_button = QPushButton('Send')
+        self.send_file_button = QPushButton('Send File')
+
+        # Stwórz layout
+        vbox = QVBoxLayout()
+        vbox.addWidget(self.message_history)
+
+        hbox = QHBoxLayout()
+        hbox.addWidget(self.message_input)
+        hbox.addWidget(self.send_button)
+        hbox.addWidget(self.send_file_button)
+
+        vbox.addLayout(hbox)
+
+        # Ustaw layout
+        central_widget = QWidget()
+        central_widget.setLayout(vbox)
+        self.setCentralWidget(central_widget)
+
+        # Połącz sygnały z slotami
+        self.send_button.clicked.connect(self.send_message)
+        self.send_file_button.clicked.connect(self.send_file)
+
+        # Utwórz gniazdo sieciowe
+        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server_socket.connect(('localhost', 9999))
+
+        # Utwórz wątek odbierający dane z serwera
+        self.receive_thread = threading.Thread(target=self.handle_server)
+        self.receive_thread.start()
+
+    def handle_server(self):
+        while True:
+            try:
+                # Użyj funkcji select, aby oczekiwać na dane do odczytu z serwera
+                ready_to_read, _, _ = select.select([self.server_socket], [], [], 0.1)
+                if self.server_socket in ready_to_read:
+                    message = self.server_socket.recv(1024).decode('utf-8')
+                    if message == 'q':
+                        break
+
+                    # Dodaj otrzymaną wiadomość do historii wiadomości
+                    self.message_history.append(message)
+            except:
+                break
+
+        self.server_socket.close()
+        sys.exit()
+
+    def send_message(self):
+        # Pobierz treść wiadomości z pola tekstowego
+        message = self.message_input.text()
 
         if message == 'q':
-            break
-        elif message.startswith('send_file'):
-            # Pobierz nazwę pliku z wiadomości
-            filename = message.split()[1]
+            # Zamknij połączenie sieciowe i wyjdź z aplikacji
+            self.server_socket.send(message.encode('utf-8'))
+            self.server_socket.close()
+            sys.exit()
+        elif message:
+            # Wyślij wiadomość do serwera
+            self.server_socket.send(message.encode('utf-8'))
 
-            # Wczytaj plik graficzny
-            img = Image.open(filename)
+        # Wyczyść pole tekstowe
+        self.message_input.clear()
 
-            # Konwertuj obraz na ASCII-art
-            ascii_art = convert_to_ascii_art(img)
+    def send_file(self):
+        # Pobierz nazwę pliku z pola tekstowego
+        #filename = self.message_input.text()
+        filename, _ = QFileDialog.getOpenFileName(self, 'Przeglądaj', '/', "Obrazy (*.png *.jpg *.jpeg)")
+        #print(filename)
+        if filename:
+            try:
+                # Wczytaj plik graficzny
+                img = Image.open(filename)
 
-            # Prześlij ASCII-art do serwera
-            #print(ascii_art)
-            for line in ascii_art.split('\n'):
-                if line:
-                    server_socket.send(line.encode('utf-8'))
-                    time.sleep(0.02)  # opóźnienie dla lepszej czytelności
-                    #server_socket.send('\n'.encode('utf-8'))
-        else:
-            server_socket.send(message.encode('utf-8'))
+                # Konwertuj obraz na ASCII-art
+                ascii_art = convert_to_ascii_art(img)
 
-    server_socket.close()
+                # Prześlij ASCII-art do serwera
+                for line in ascii_art.split('\n'):
+                    if line:
+                        self.server_socket.send(line.encode('utf-8'))
+                        time.sleep(0.02)  # opóźnienie dla lepszej czytelności
+            except:
+                print("aaa")
+
+        # Wyczyść pole tekstowe
+        self.message_input.clear()
 
 if __name__ == '__main__':
-    main()
+    app = QApplication(sys.argv)
+
+    main_window = ChatWindow()
+    main_window.show()
+
+    sys.exit(app.exec_())
